ae4261b5c4bf68e21f7bd13dff81e34b4e8d9f05
Fernando Calisto <fernandocalisto27@gmail.com>
2025-03-28 15:56:51 -0300

    Jogo Do Número Secreto em Java!

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner leitura = new Scanner(System.in);
+        int numeroSecreto = new Random().nextInt(100);
+        int chute = 0;
+        int tentativas = 1;
+
+        System.out.println(numeroSecreto);
+
+        System.out.println("Bem-vindo ao Jogo do Número Secreto! Você tem 5 tentativas para acertar o número entre 0 e 100!");
+
+        while (true) {
+
+            System.out.printf("Insira sua tentativa N°%d: ",tentativas);
+            chute = leitura.nextInt();
+            if (chute == numeroSecreto){
+                System.out.println("Parabéns!!! Você acertou o número Secreto em apenas " + tentativas + " tentativas!");
+                break;
+            }
+            if (tentativas >= 5){
+                System.out.println("As suas chances acabaram :( O número Secreto era " + numeroSecreto);
+                break;
+            }
+            tentativas++;
+
+        }
+    }
+}
